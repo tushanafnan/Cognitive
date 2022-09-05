9688ff62ccb308ded96358218214c1657db5e79a
Md Sultanul Arefin Afnan (Tushan) <tush.pay@gmail.com>
2022-09-05 23:58:07 +0600

    User Updated

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -6,8 +7,10 @@ import { useHistory } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import Footer from './../Footer/Footer';
+import { useParams } from 'react-router-dom';
 
 const Reports = () => {
+  const { userDefine } = useParams();
   const history = useHistory();
     const [report, setReport] = useState([]);
 
@@ -15,10 +18,15 @@ const Reports = () => {
         const reports = database.ref("Reports");
         reports.on("value", (snapshot) => {
           const report = snapshot.val();
-          const Reports= Object.values(report);
-          setReport(Reports);
-        });
-      }, []);
+          const users = Object.values(report).map((data)=>  data.user );
+          const local = JSON.parse(localStorage.getItem("user"));
+          if (local ==  users) {
+           setReport( Object.values(report));
+        }
+        
+         });
+       }, []);
+      
       const handleShowReportDetails = (fileName) => {
         history.push(`/reportdetails/${fileName}`);
       };
