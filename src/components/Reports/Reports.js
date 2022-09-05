9eb8b18c1f99c2e51f564a3c7d2f02f20c5f68fb
Md Sultanul Arefin Afnan (Tushan) <tush.pay@gmail.com>
2022-09-06 00:00:32 +0600

    Reports Updated

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -7,53 +7,50 @@ import { useHistory } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import Footer from './../Footer/Footer';
-import { useParams } from 'react-router-dom';
-
 const Reports = () => {
-  const { userDefine } = useParams();
   const history = useHistory();
-    const [report, setReport] = useState([]);
+  const [report, setReport] = useState([]);
 
-    useEffect(() => {
-        const reports = database.ref("Reports");
-        reports.on("value", (snapshot) => {
-          const report = snapshot.val();
-          const users = Object.values(report).map((data)=>  data.user );
-          const local = JSON.parse(localStorage.getItem("user"));
-          if (local ==  users) {
-           setReport( Object.values(report));
-        }
-        
-         });
-       }, []);
-      
-      const handleShowReportDetails = (fileName) => {
-        history.push(`/reportdetails/${fileName}`);
-      };
-    return (
-       <div>
-        <div>
-            {Object.values(report).map((report, index) => {
-                  const {  fileName } = report;
-                  return (
-                    
-                    <Box className="blog-article-right-part blog-article-part" key={fileName}>
-                    <Button
-                      variant="contained"
-                      size="small"
-                      onClick={() => {
-                        handleShowReportDetails(fileName);
-                      }}
-                    >
-                     {fileName}
-                    </Button>
-                    </Box>
-                    
-                    )})}
-                    </div>
-            <Footer/>
-            </div>
-    );
+  useEffect(() => {
+    const reports = database.ref("Reports");
+    reports.on("value", (snapshot) => {
+      const report = snapshot.val();
+      const users = Object.values(report).map((data) => data.user);
+      const local = JSON.parse(localStorage.getItem("user"));
+      if (local == users) {
+        setReport(Object.values(report));
+      }
+
+    });
+  }, []);
+
+  const handleShowReportDetails = (fileName) => {
+    history.push(`/reportdetails/${fileName}`);
+  };
+  return (
+    <div>
+      <div>
+        {Object.values(report).map((report, index) => {
+          const { fileName } = report;
+          return (
+
+            <Box className="blog-article-right-part blog-article-part" key={fileName}>
+              <Button
+                variant="contained"
+                size="small"
+                onClick={() => {
+                  handleShowReportDetails(fileName);
+                }}
+              >
+                {fileName}
+              </Button>
+            </Box>
+          )
+        })}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Reports;
